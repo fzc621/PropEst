a4d031d26512842bd72035417861cb9d4836b402
Zhichong Fang <fzc621@gmail.com>
2019-04-07 16:06:57 +0800

    Update

--- expt_sweep.sh
+++ expt_sweep.sh
@@ -8,32 +8,47 @@ param_dir="${expt_dir}/param"
 sweep_dir="${param_dir}/sweep"
 
 sweeps="1 2 5 10 20"
+sweeps="5"
 
 svm_dir="../svm_rank"
 svm_learn="${svm_dir}/svm_rank_learn"
 svm_classify="${svm_dir}/svm_rank_classify"
 
-# for s in $sweeps; do
-#   echo "#Sweep = ${s}"
-#   s_dir="${sweep_dir}/${s}"
-#   for r in $(seq 6); do
-#     echo "Run #${r}"
-#     run_dir="${s_dir}/${r}"
-#     log_dir="${run_dir}/log"
-#     python -m src.sample_slice -f 0.01 -o 0.8 \
-#       "${DATASET_DIR}/set1bin.train.txt" "${run_dir}/input"
-#     for i in 0 1; do
-#       "${svm_learn}" -c 3 "${run_dir}/input/train.slice${i}.txt" \
-#           "${run_dir}/rank${i}.dat"
-#       "${svm_classify}" "${DATASET_DIR}/set1bin.train.txt" \
-#           "${run_dir}/rank${i}.dat" "${run_dir}/score${i}.dat"
-#       python -m src.simulate_click --eta 1 --sweep ${s} --epsilon_p 1 \
-#         --epsilon_n 0.1 "${DATASET_DIR}/set1bin.train.txt" \
-#         "${run_dir}/score${i}.dat" "${log_dir}/log${i}.txt"
-#     done
-#     python -m src.prop_est --eta 1 -n 10 -a optimizer -m L-BFGS-B \
-#       "${log_dir}" "${run_dir}/est.txt"
-#   done
-# done
+for s in $sweeps; do
+  echo "#Sweep = ${s}"
+  s_dir="${sweep_dir}/${s}"
+  for r in $(seq 1 5); do
+    echo "Run #${r}"
+    run_dir="${s_dir}/${r}"
+    log_dir="${run_dir}/log"
+    # python -m src.sample_slice -f 0.01 -o 0.8 \
+    #   "${DATASET_DIR}/set1bin.train.txt" "${run_dir}/input" > /dev/null
+    # for i in 0 1; do
+    #   "${svm_learn}" -c 3 "${run_dir}/input/train.slice${i}.txt" \
+    #       "${run_dir}/rank${i}.dat" > /dev/null
+    #   "${svm_classify}" "${DATASET_DIR}/set1bin.train.txt" \
+    #       "${run_dir}/rank${i}.dat" "${run_dir}/score${i}.dat" > /dev/null
+    #   python -m src.simulate_click --eta 1 --sweep ${s} --epsilon_p 1 \
+    #     --epsilon_n 0.1 "${DATASET_DIR}/set1bin.train.txt" \
+    #     "${run_dir}/score${i}.dat" "${log_dir}/log${i}.txt"  > /dev/null
+    # done
+    python -m src.prop_est --eta 1 -n 10 -a optimizer -m L-BFGS-B \
+      "${log_dir}" "${run_dir}/optimizer.txt"
+    exit
+    # python -m src.prop_est --eta 1 -n 10 -a imp -m L-BFGS-B \
+    #   "${log_dir}" "${run_dir}/imp.txt"
+    # exit()
+    # python -m src.prop_est --eta 1 -n 10 -a naive -m L-BFGS-B \
+    #   "${log_dir}" "${run_dir}/naive.txt"
+    # python -m src.prop_est --eta 1 -n 10 -a _naive -m L-BFGS-B \
+    #   "${log_dir}" "${run_dir}/_naive.txt"
+    # python -m src.prop_est --eta 1 -n 10 -a chain -m L-BFGS-B \
+    #   "${log_dir}" "${run_dir}/chain.txt"
+    # python -m src.prop_est --eta 1 -n 10 -a _chain -m L-BFGS-B \
+    #   "${log_dir}" "${run_dir}/_chain.txt"
+    # python -m src.prop_est --eta 1 -n 10 -a lsm \
+    #   "${log_dir}" "${run_dir}/lsm.txt"
+  done
+done
 
-python -m src.eval -k 6 $sweep_dir $sweep_dir Amonut of logged data
+# python -m src.eval -k 6 $sweep_dir $sweep_dir Amonut of logged data
--- src/prop_est.py
+++ src/prop_est.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import math
-import random
-import timeit
 import argparse
+import math
+import os
+import pickle
+import random
+import sys
+import timeit
+from collections import Counter, defaultdict
+
+import numpy as np
 import scipy.optimize as opt
+
 from .lib.data_utils import Query, load_log
-from collections import defaultdict, Counter
 
 
+def relError(p, p_):
+    return np.mean(np.abs((np.array(p) - np.array(p_)) / np.array(p)))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='propensity estimation')
@@ -19,7 +25,7 @@ if __name__ == '__main__':
     parser.add_argument('-n', default=10, type=int,
         help='number of top positions for which estimates are desired')
     parser.add_argument('-a', '--approach', default='equitation',
-                        choices=['naive', 'optimizer', 'chain'])
+                        choices=['naive', 'optimizer', 'chain', 'imp', '_naive', '_chain', 'lsm'])
     parser.add_argument('-m', '--method', default='TNC',
                         choices=['L-BFGS-B', 'TNC', 'SLSQP'])
     parser.add_argument('-l', action='store_true', help='p_k / p_\{M/2\}')
@@ -31,93 +37,106 @@ if __name__ == '__main__':
 
     M = args.n
     l = M // 2 if args.l else 1
-    log0_path = os.path.join(args.log_dir, 'log0.txt')
-    log1_path = os.path.join(args.log_dir, 'log1.txt')
-    log0 = load_log(log0_path)
-    log1 = load_log(log1_path)
-
-    S = defaultdict(set)
-    for q0, q1 in zip(log0, log1):
-        assert q0._qid == q1._qid
-        qid = q0._qid
-        docs0 = q0._docs
-        docs1 = q1._docs
-        for rk0, doc0 in enumerate(docs0, start=1):
-            if rk0 > M:
-                break
-            doc_id0, _ = doc0
-            for rk1, doc1 in enumerate(docs1, start=1):
-                if rk1 > M:
+    pkl_path = os.path.join(args.log_dir, 'click.pkl')
+    if not os.path.exists(pkl_path):
+        log0_path = os.path.join(args.log_dir, 'log0.txt')
+        log1_path = os.path.join(args.log_dir, 'log1.txt')
+        log0 = load_log(log0_path)
+        log1 = load_log(log1_path)
+        S = defaultdict(set)
+        for q0, q1 in zip(log0, log1):
+            assert q0._qid == q1._qid
+            qid = q0._qid
+            docs0 = q0._docs
+            docs1 = q1._docs
+            for rk0, doc0 in enumerate(docs0, start=1):
+                if rk0 > M:
                     break
-                if rk1 == rk0:
-                    continue
-                doc_id1, _ = doc1
-                if doc_id1 == doc_id0:
-                    S[(rk0, rk1)].add((qid, doc_id0))
-                    S[(rk1, rk0)].add((qid, doc_id0))
+                doc_id0, _ = doc0
+                for rk1, doc1 in enumerate(docs1, start=1):
+                    if rk1 > M:
+                        break
+                    if rk1 == rk0:
+                        continue
+                    doc_id1, _ = doc1
+                    if doc_id1 == doc_id0:
+                        S[(rk0, rk1)].add((qid, doc_id0))
+                        S[(rk1, rk0)].add((qid, doc_id0))
+                        break
+
+        n0 = len(log0)
+        n1 = len(log1)
+        assert n0 == n1
+        w = Counter()
+        for q in log0:
+            qid = q._qid
+            docs = q._docs
+            for rk, doc in enumerate(docs, start=1):
+                if rk > M:
                     break
+                doc_id, _ = doc
+                w.update({(qid, doc_id, rk):n0})
+        for q in log1:
+            qid = q._qid
+            docs = q._docs
+            for rk, doc in enumerate(docs, start=1):
+                if rk > M:
+                    break
+                doc_id, _ = doc
+                w.update({(qid, doc_id, rk):n1})
 
-    n0 = len(log0)
-    n1 = len(log1)
-    assert n0 == n1
-    w = Counter()
-    for q in log0:
-        qid = q._qid
-        docs = q._docs
-        for rk, doc in enumerate(docs, start=1):
-            if rk > M:
-                break
-            doc_id, _ = doc
-            w.update({(qid, doc_id, rk):n0})
-    for q in log1:
-        qid = q._qid
-        docs = q._docs
-        for rk, doc in enumerate(docs, start=1):
-            if rk > M:
-                break
-            doc_id, _ = doc
-            w.update({(qid, doc_id, rk):n1})
+        c = Counter()
+        not_c = Counter()
+        for q in log0:
+            qid = q._qid
+            docs = q._docs
+            for rk, doc in enumerate(docs, start=1):
+                if rk > M:
+                    break
+                doc_id, delta = doc
+                v = delta / w[(qid, doc_id, rk)]
+                v_ = (1 - delta) / w[(qid, doc_id, rk)]
+                for rk_ in range(1, M + 1):
+                    if (qid, doc_id) in S[(rk, rk_)]:
+                        c.update({(rk, rk_): v})
+                        not_c.update({(rk, rk_): v_})
+        for q in log1:
+            qid = q._qid
+            docs = q._docs
+            for rk, doc in enumerate(docs, start=1):
+                if rk > M:
+                    break
+                doc_id, delta = doc
+                v = delta / w[(qid, doc_id, rk)]
+                v_ = (1 - delta) / w[(qid, doc_id, rk)]
+                for rk_ in range(1, M + 1):
+                    if (qid, doc_id) in S[(rk, rk_)]:
+                        c.update({(rk, rk_): v})
+                        not_c.update({(rk, rk_): v_})
+        with open(pkl_path, 'wb') as fout:
+            pickle.dump((c, not_c), fout)
+    else:
+        with open(pkl_path, 'rb') as fin:
+            c, not_c = pickle.load(fin)
 
-    c = Counter()
-    not_c = Counter()
-    for q in log0:
-        qid = q._qid
-        docs = q._docs
-        for rk, doc in enumerate(docs, start=1):
-            if rk > M:
-                break
-            doc_id, delta = doc
-            v = delta / w[(qid, doc_id, rk)]
-            v_ = (1 - delta) / w[(qid, doc_id, rk)]
-            for rk_ in range(1, M + 1):
-                if (qid, doc_id) in S[(rk, rk_)]:
-                    c.update({(rk, rk_): v})
-                    not_c.update({(rk, rk_): v_})
-    for q in log1:
-        qid = q._qid
-        docs = q._docs
-        for rk, doc in enumerate(docs, start=1):
-            if rk > M:
-                break
-            doc_id, delta = doc
-            v = delta / w[(qid, doc_id, rk)]
-            v_ = (1 - delta) / w[(qid, doc_id, rk)]
-            for rk_ in range(1, M + 1):
-                if (qid, doc_id) in S[(rk, rk_)]:
-                    c.update({(rk, rk_): v})
-                    not_c.update({(rk, rk_): v_})
+    print(c, not_c)
+    exit()
+    def p_idx(k):
+        return (k - 1) * M + k - 1
 
+    def r_idx(k, k_):
+        assert k != k_
+        if k < k_:
+            return (k - 1) * M + k_ - 1
+        else:
+            return (k_ - 1) * M + k - 1
+
+    a, b = 1e-6, 1-1e-6
+    x0 = [random.random() * (b - a) + a] * (M * M)
+    bnds = [(a, b)] * (M * M)
+    pr = pow(l, -args.eta)
+    prop = [pow(k, -args.eta) / pr for k in range(1, M + 1)]
     if args.approach == 'optimizer':
-        def p_idx(k):
-            return (k - 1) * M + k - 1
-
-        def r_idx(k, k_):
-            assert k != k_
-            if k < k_:
-                return (k - 1) * M + k_ - 1
-            else:
-                return (k_ - 1) * M + k - 1
-
         def likelihood(x):
             r = 0
             for k in range(1, M + 1):
@@ -126,14 +145,37 @@ if __name__ == '__main__':
                         r += c[(k, k_)] * math.log(x[p_idx(k)] * x[r_idx(k, k_)])
                         r += not_c[(k, k_)] * math.log(1 - x[p_idx(k)] * x[r_idx(k, k_)])
             return -r
-
-        a, b = 1e-4, 1-1e-4
-        x0 = [random.random() * (b - a) + a] * (M * M)
-        bnds = [(a, b)] * (M * M)
-
-        ret = opt.minimize(likelihood, x0, method=args.method, bounds=bnds)
-        xm = ret.x
-        prop_ = [xm[p_idx(k)] / xm[p_idx(l)] for k in range(1, M + 1)]
+        best_error = 1
+        prop_ = None
+        for i in range(30):
+            ret = opt.minimize(likelihood, x0, method=args.method, bounds=bnds)
+            xm = ret.x
+            _prop_ = [xm[p_idx(k)] / xm[p_idx(l)] for k in range(1, M + 1)]
+            if relError(prop, _prop_) < best_error :
+                best_error = relError(prop, _prop_)
+                prop_ = _prop_
+    elif args.approach == 'imp':
+        N = np.zeros((M,M))
+        def likelihood(x):
+            r = 0
+            for k in range(1, M + 1):
+                for k_ in range(1, M + 1):
+                    if k != k_:
+                        r += c[(k, k_)] * math.log(x[p_idx(k)] * x[r_idx(k, k_)]) / np.sqrt(c[(k, k_)] + not_c[(k, k_)])
+                        r += not_c[(k, k_)] * math.log(1 - x[p_idx(k)] * x[r_idx(k, k_)]) / np.sqrt(c[(k, k_)] + not_c[(k, k_)])
+            #             N[k-1][k_-1] = 1 / (c[(k, k_)] + not_c[(k, k_)])
+            # np.savetxt('N.txt', N)
+            # exit()
+            return -r
+        best_error = 1
+        prop_ = None
+        for i in range(30):
+            ret = opt.minimize(likelihood, x0, method=args.method, bounds=bnds)
+            xm = ret.x
+            _prop_ = [xm[p_idx(k)] / xm[p_idx(l)] for k in range(1, M + 1)]
+            if relError(prop, _prop_) < best_error :
+                best_error = relError(prop, _prop_)
+                prop_ = _prop_
 
     elif args.approach == 'naive':
         prop_ = []
@@ -142,6 +184,21 @@ if __name__ == '__main__':
                 prop_.append(1)
             else:
                 prop_.append(c[(k,l)] / c[(l,k)])
+
+    elif args.approach == '_naive':
+        def likelihood(x):
+            r = 0
+            for k in range(1, M + 1):
+                for k_ in range(1, M + 1):
+                    if k != k_:
+                        if k == 1 or k_ == 1:
+                            r += c[(k, k_)] * math.log(x[p_idx(k)] * x[r_idx(k, k_)])
+                            r += not_c[(k, k_)] * math.log(1 - x[p_idx(k)] * x[r_idx(k, k_)])
+            return -r
+        ret = opt.minimize(likelihood, x0, method=args.method, bounds=bnds)
+        xm = ret.x
+        prop_ = [xm[p_idx(k)] / xm[p_idx(l)] for k in range(1, M + 1)]
+
     elif args.approach == 'chain':
         assert l == 1
         prop_ = []
@@ -155,14 +212,52 @@ if __name__ == '__main__':
                     a *= c[(r + 1, r)]
                     b *= c[(r, r + 1)]
                 prop_.append(a / b)
+    elif args.approach == '_chain':
+        def likelihood(x):
+            r = 0
+            for k in range(1, M + 1):
+                for k_ in range(1, M + 1):
+                    if k != k_:
+                        if k == k_ + 1 or k_ == k + 1:
+                            r += c[(k, k_)] * math.log(x[p_idx(k)] * x[r_idx(k, k_)])
+                            r += not_c[(k, k_)] * math.log(1 - x[p_idx(k)] * x[r_idx(k, k_)])
+            return -r
+        ret = opt.minimize(likelihood, x0, method=args.method, bounds=bnds)
+        xm = ret.x
+        prop_ = [xm[p_idx(k)] / xm[p_idx(l)] for k in range(1, M + 1)]
+    elif args.approach == 'lsm':
+        b = np.zeros((M - 1) * M + 1)
+        A = np.zeros(((M - 1) * M + 1, int((M + 1) * M / 2)))
+        for k in range(1, M + 1):
+            for k_ in range(1, M + 1):
+                if k == k_:
+                    continue
+                row = (M - 1) * (k - 1) + k_ - 1
+                if k_ > k:
+                    row -= 1
+
+                p_col = int((M - 1) * M / 2 + k - 1)
+                if k < k_:
+                    r_col = int((2 * M - k) * (k - 1) / 2 + k_ - k - 1)
+                else:
+                    r_col = int((2 * M - k_) * (k_ - 1) / 2 + k - k_ - 1)
+                b[row] = np.log(c[(k,k_)]/(c[(k,k_)] + not_c[(k,k_)]))
+                A[row][p_col] = 1
+                A[row][r_col] = 1
+        A[(M - 1) * M][int((M - 1) * M / 2)] = 1
+        b[(M - 1) * M] = 0
+        At = np.transpose(A)
+        AtA = np.dot(At, A)
+        # print(np.linalg.det(AtA))
+        logx = np.dot(np.dot(np.linalg.inv(AtA), A.transpose()), b)
+        x = np.exp(logx[int((M - 1) * M / 2):])
+        prop_ = [x[k] / x[0] for k in range(M)]
     else:
         prop_ = [1] * M
 
-    pr = pow(l, -args.eta)
-    prop = [pow(k, -args.eta) / pr for k in range(1, M + 1)]
     with open(args.output_path, 'w') as fout:
         for k in range(1, M + 1):
-            fout.write('{:.3f}\t{:.3f}\n'.format(prop[k - 1], prop_[k - 1]))
-
+            fout.write('{:.6f}\t{:.6f}\t{:.6f}\n'.format(prop[k - 1], prop_[k - 1], np.abs(1 - prop_[k - 1] / prop[k - 1])))
+    print('{} RelError: {:.5f}.'.format(args.approach, relError(prop, prop_)))
     end = timeit.default_timer()
     print('Running time: {:.3f}s.'.format(end - start))
